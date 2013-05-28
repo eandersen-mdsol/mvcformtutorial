e5351f8a559ecd6ee02036e5ae9180e292b6d3a3
Ed Andersen <eandersen@mdsol.com>
2013-05-28 13:54:19 +0900

    Finished part 1

--- MVCFormsExample/ViewModels/BookViewModel.cs
+++ MVCFormsExample/ViewModels/BookViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCFormsExample.ViewModels
 {
     public class BookViewModel
     {
+        [Required]
         public Guid Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
         public string Author { get; set; }
-        public DateTime DatePublished { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name = "Date published")]
+        public DateTime? DatePublished { get; set; }
+        
+        [Range(1,5)]
+        [Display(Name = "Rating (1-5)")]
         public int Rating { get; set; }
     }
 }
